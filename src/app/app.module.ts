d62b826084e521eb29f7e56fc71c5b0283355e99
junaid <junaidvinu133@gmail.com>
2024-10-14 19:06:15 +0530

    feat : add necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderNavbarComponent } from './components/header-navbar/header-navbar.component';
+import { PromptComponent } from './components/prompt/prompt.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderNavbarComponent,
+    PromptComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
