f77adb29c6bdbb1bbd0464df64322cafd0ce67fa
junaid <junaidvinu133@gmail.com>
2024-10-15 16:50:37 +0530

    feat : add  skeleton loader product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
- product =[{
-  
- }]
+export class ProductComponent implements OnInit {
+   data=false
+  ngOnInit(): void {
+    setTimeout(() => {
+       this.data=true
+    }, 1000);
+  }
 }
